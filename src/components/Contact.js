bcafe7233546f179af479222da77508d8d0428f4
Yuuka Motobayashi <yuukanon@gmail.com>
2020-06-29 11:52:00 +0200

    completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,23 +7,25 @@ class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      online: false,
+      online: props.online,
     };
   }
+
+  changeStatus = () => {
+    this.setState({ online: !this.state.online })
+  }
+
   render() {
+    console.log(this.state.online)
     return(
       <div className="Contact">
         <img src={this.props.avatar} alt={this.props.avatar} className="avatar" />
         <p>
           <div className="name">{this.props.name}</div>
-          <div className="status">
-              <div className= { this.state.online ? 'status-offline' : 'status-online' } 
-              onClick = {event => {
-                const statusMark = !this.state.online;
-                this.setState({online: statusMark});
-              }}></div>
+          <div className= "status" onClick = {this.changeStatus}>
+              <div className = {this.state.online ? 'status-offline' : 'status-online'}></div>
               <div className="status-text">
-                {this.props.online ? 'offline' : 'online'}
+                {this.state.online === false ? 'online' : 'offline'}
               </div>
               </div>
             </p>
